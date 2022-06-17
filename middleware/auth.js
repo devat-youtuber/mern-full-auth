ec148424c70fa19c1cae058f43d6fd82012370d1
Farida Hasanova <99580486+faridasLab@users.noreply.github.com>
2022-06-17 11:17:10 +0400

    update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 
 const auth = (req, res, next) => {
     try {
-        const token = req.header("Authorization")
-        if(!token) return res.status(400).json({msg: "Invalid Authentication."})
+        const token = req.header("Authorization");
+        if(!token) return res.status(400).json({msg: "Invalid Authentication."});
 
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if(err) return res.status(400).json({msg: "Invalid Authentication."})
+            if(err) return res.status(400).json({msg: "Invalid Authentication."});
 
-            req.user = user
-            next()
-        })
+            req.user = user;
+            next();
+        });
     } catch (err) {
-        return res.status(500).json({msg: err.message})
-    }
-}
+        return res.status(500).json({msg: err.message});
+    };
+};
 
-module.exports = auth
+module.exports = auth;
